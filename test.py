2193f351706db9a9ac675a410f5be3b962e8ae72
mustafa atik <muatik@gmail.com>
2015-02-13 15:47:43 +0200

    crawler demo file is created

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+from crawlers import *
+
+if __name__ == '__main__':
+    print("Testing crawlers by fetching the stock ISCTR:")
+    print Uzmanpara.getStock("THYAO")
+    print Bigpara.getStock("THYAO")
+    print MarketWatch.getStock("THYAO")
+    print MarketWatch.getParity("usd-try")
+    print Google.getParity("usdtry")
+    print MarketWatch.getParity("eurusd")
+    print Google.getParity("eurusd")
+    print MarketWatch.getOil()
+    print TWSJ.getOil()
