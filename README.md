c31a17aeb788b87ef7a248977b30abe86aaca24e
mustafa atik <muatik@gmail.com>
2015-02-16 11:49:19 +0200

    installing by pip is tested

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # paramara
 stocks and currency crawler
+
+
+This is an experimental work about detectiong correlations and causations
+in stock prices by looking at stock prices, commodities, currencies etc...
--- setup.py
+++ setup.py
@@ -4,18 +4,13 @@ import sys
 
 from setuptools import find_packages, setup
 
-long_description = """
-This is an experimental work about detectiong correlations and causations
-in stock prices by looking at stock prices, commodities, currencies etc...
-"""
-
 setup(
     name="paramara",
     version="0.1",
     packages=find_packages(),
     author="Mustafa Atik",
     author_email="muatik@gmail.com",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     description='stock price crawler',
     license="MIT",
     url="https://github.com/muatik/paramara",
